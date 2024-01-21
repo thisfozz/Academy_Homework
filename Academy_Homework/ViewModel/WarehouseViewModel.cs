4b03a4d60380d947820df0c4ec40126aa6b1eb63
thisfozz <kfcfozz@vk.com>
2024-01-21 16:11:17 +0500

    Добавил проверку на корректность ввода номера типа продукта и обернул вставку в транзацию

--- Academy_Homework/ViewModel/WarehouseViewModel.cs
+++ Academy_Homework/ViewModel/WarehouseViewModel.cs
@@ -20,6 +20,7 @@ namespace Academy_Homework.ViewModel
 
         public ICommand ConnectCommandButton { get; } // Подключение к базе данных
         public ICommand AddProductCommand { get; } // Добавление товара
+        public ICommand AddProductTypes { get; } // Добавление типа продукта
 
         public WarehouseViewModel()
         {
@@ -28,6 +29,7 @@ namespace Academy_Homework.ViewModel
 
             ConnectCommandButton = new DelegateCommand(ConnectToDatabase, (_) => true);
             AddProductCommand = new DelegateCommand(AddProduct, (_) => true);
+            AddProductTypes = new DelegateCommand(AddProductType, (_) => true);
         }
 
         // Временное решение с передачей ID. (может получать через SELECT id FROM Product ORDER BY id DESC LIMIT 1; +1 сделав поле readonly)
@@ -101,6 +103,20 @@ namespace Academy_Homework.ViewModel
             }
         }
 
+        private string _productType;
+        public string ProductType
+        {
+            get { return _productType; }
+            set
+            {
+                if (_productType != value)
+                {
+                    _productType = value;
+                    OnPropertyChanged(nameof(ProductType));
+                }
+            }
+        }
+
         private void ConnectToDatabase(object obj)
         {
 
@@ -136,20 +152,100 @@ namespace Academy_Homework.ViewModel
             }
         }
 
+        int getLastIndex(string sqlQuery)
+        {
+            using var command = new NpgsqlCommand(sqlQuery, connection);
+            using var reader = command.ExecuteReader();
+
+            int lastIndex = -1;
+
+            if (reader.Read())
+            {
+                lastIndex = Convert.ToInt32(reader["id"]);
+            }
+            lastIndex += 1;
+
+            return lastIndex;
+        }
+
+        private bool IsProductTypeValid(int productTypeId)
+        {
+            int maxProductTypeId = getLastIndex("SELECT id FROM ProductTypes ORDER BY id DESC");
+            return productTypeId >= 1 && productTypeId <= maxProductTypeId;
+        }
+
         private void AddProduct(object obj)
         {
             ExecuteIfConnectionOpen(() =>
             {
-                var commandText = "INSERT INTO Products VALUES(@name, @type_id, @quantity, @cost_price)";
+                NpgsqlTransaction transaction = connection.BeginTransaction();
+                try
+                {
+                    int lastIndex = getLastIndex("SELECT id FROM Products ORDER BY id DESC");
 
-                using NpgsqlCommand command = new NpgsqlCommand(commandText, connection);
+                    if (IsProductTypeValid(ProductTypeID))
+                    {
+                        var commandText = "INSERT INTO Products VALUES(@id, @name, @type_id, @quantity, @cost_price)";
 
-                command.Parameters.AddWithValue("@name", ProductName);
-                command.Parameters.AddWithValue("@type_id", ProductTypeID);
-                command.Parameters.AddWithValue("@quantity", ProductQuantity);
-                command.Parameters.AddWithValue("@cost_price", ProductCostPrice);
+                        using NpgsqlCommand command = new NpgsqlCommand(commandText, connection);
 
-                command.ExecuteNonQuery();
+                        command.Parameters.AddWithValue("@id", lastIndex);
+                        command.Parameters.AddWithValue("@name", ProductName);
+                        command.Parameters.AddWithValue("@type_id", ProductTypeID);
+                        command.Parameters.AddWithValue("@quantity", ProductQuantity);
+                        command.Parameters.AddWithValue("@cost_price", ProductCostPrice);
+
+                        command.ExecuteNonQuery();
+                        transaction.Commit();
+
+                        MessageBox.Show("Данные успешно добавлены");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Веденный номер товара не существует");
+                    }
+                } 
+                catch(Exception ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show($"При добавлении данных возникла ошибка - {ex.Message}");
+                }
+                finally
+                {
+                    transaction.Dispose();
+                }
+            });
+        }
+
+        private void AddProductType(object obj)
+        {
+            ExecuteIfConnectionOpen(() =>
+            {
+                NpgsqlTransaction transaction = connection.BeginTransaction();
+                try
+                {
+                    var commandText = "INSERT INTO ProductTypes VALUES(@id, @type_name)";
+                    int lastIndex = getLastIndex("SELECT id FROM ProductTypes ORDER BY id DESC");
+
+                    using NpgsqlCommand command = new NpgsqlCommand(commandText, connection);
+
+                    command.Parameters.AddWithValue("@id", lastIndex);
+                    command.Parameters.AddWithValue("@type_name", ProductType);
+
+                    command.ExecuteNonQuery();
+                    transaction.Commit();
+
+                    MessageBox.Show("Данные успешно добавлены");
+                } 
+                catch(Exception ex)
+                {
+                    transaction.Rollback();
+                    MessageBox.Show($"При добавлении данных возникла ошибка - {ex.Message}");
+                }
+                finally
+                {
+                    transaction.Dispose();
+                }
             });
         }
 
